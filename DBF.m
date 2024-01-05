6ed51da89cdfe7d5822018b928ce88f340e9f5b6
lewsilver <cyinliu@163.com>
2024-01-05 20:30:53 +0800

    Signal Model and DBF

---
+++ DBF.m
@@ -0,0 +1,27 @@
+% DBF
+% aS = pinv(a'a)a' X
+
+
+%% Initial signal
+T = 1;
+snsrMap = [1; 2; 3; 4; 5; 6; 7; 8; 9; 10; 11; 12;];
+tThetas = [5; 10];
+[X, tA] = arrayModel(snsrMap, T, tThetas);
+tS = pinv(tA' * tA) * tA' * X;
+
+%% Initial scan grid
+pThetas = -64:0.5:63.5;
+pThetas = pThetas .';
+pA = exp(1i * pi * snsrMap* sind(pThetas).');
+pA = pA ./ norm(pA(:, 1));
+
+%% DBF
+pDBF = zeros(size(pThetas));
+for i = 1: length(pThetas)
+    pDBF(i) = pinv(pA(:, i)' * pA(:, i)) * pA(:, i)' * X;
+end
+
+
+%% Figure;
+figure;
+plot(pThetas, 20 * log10(abs(pDBF) / max(abs(pDBF))));
---
+++ arrayModel.m
@@ -0,0 +1,21 @@
+function [X, tA] = arrayModel(snsrMap, T, target)
+% X = As + e
+% snsrMap: Sensor array, d = lambda / 2
+% T      : Snapshot
+% target : Targets at angle thetas
+
+tA = exp(1i * pi * snsrMap* sind(target).');
+tA = tA ./ norm(tA(:, 1));
+
+pd = makedist('Normal','mu',10,'sigma',2);
+s = random(pd, size(target));
+
+noiseSNR = 15;
+
+X = zeros(length(snsrMap), T);
+for i = 1: T
+	X(:, i) = awgn(tA * s, noiseSNR);
+end
+
+end
+
